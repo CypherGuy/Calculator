299bf1c6a91ef23f50a6c83a149960036a29000b
Kabir Ghai <74025064+CypherGuy@users.noreply.github.com>
2024-02-21 14:39:58 +0000

    Add basic functionality

---
+++ script.js
@@ -0,0 +1,31 @@
+a = document.getElementById("value1").value;
+b = document.getElementById("value2").value;
+
+function operate(operator, a, b) {
+    if (operator === '+') {
+        return add(a, b);
+    } else if (operator === '-') {
+        return subtract(a, b);
+    } else if (operator === '*') {
+        return multiply(a, b);
+    } else if (operator === '/') {
+        return divide(a, b);
+    }
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
